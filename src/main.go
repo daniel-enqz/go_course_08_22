448d1c1857ebd0797cc09906dfab2df15b60bb4b
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-09 10:15:55 -0500

    variable declaration

--- src/main.go
+++ src/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, pana")
+	// Declaración de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
 }
