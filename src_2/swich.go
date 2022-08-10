a691a00b3a7cce47a2f64c8f6499cf75d9f6101a
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-10 11:01:48 -0500

    Swich file added

--- src_2/swich.go
+++ src_2/swich.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	switch modulo := 5 % 2; modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
+	}
+
+	value := 100
+	switch {
+	case value > 100 && value < 200:
+		fmt.Println("Greater than 100")
+	case value < 100 && value > 0:
+		fmt.Println("Less than 100")
+	case value == 100:
+		fmt.Println("Is 100")
+	default:
+		fmt.Printf("Out of range, you put %d", value)
+	}
 }
