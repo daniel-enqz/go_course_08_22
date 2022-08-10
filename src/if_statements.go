99cbc5d025c3e4888f0ffbe0a83026ef49a537a9
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-10 10:16:41 -0500

    For loops

--- src/if_statements.go
+++ src/if_statements.go
@@ -5,10 +5,18 @@ import "fmt"
 func main() {
 	valor1 := 1
 	valor2 := 2
+	const nombre string = "UltiRequiem"
+	is_type := fmt.Sprintf("%T", nombre)
 
 	if valor1 == 1 && valor2 == 2 {
 		fmt.Println("valor1 is 1 and valor2 is 2")
 	} else {
 		fmt.Println("valor1 is not 1 and valor2 is not 2")
+	}
+
+	if is_type == "string" {
+		fmt.Println("is_type is a", is_type)
+	} else {
+		fmt.Println("is_type is not a string")
 	}
 }
