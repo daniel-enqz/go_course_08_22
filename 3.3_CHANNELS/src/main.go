0eefb8ab2e5dea012a908ce97209524c545f12a7
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-16 08:52:56 -0500

    New names

--- 3.3_CHANNELS/src/main.go
+++ 3.3_CHANNELS/src/main.go
@@ -1,13 +1,19 @@
 package main
 
-func say(text string, c chan string) {
+import (
+	"fmt"
+)
+
+func say(text string, c chan<- string) {
 	c <- text
 }
 
 func main() {
-	c := make(chan int, 1)
+	c := make(chan string, 1)
 
 	fmt.Println("Hello")
 
 	go say("Bye", c)
+
+	fmt.Println(<-c)
 }
